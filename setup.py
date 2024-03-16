10c09e02805b2c88b7d8e5b4b95a31c1031dd594
SaxenaSim <saxenasimran84@gmail.com>
2024-03-16 19:27:23 +0530

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup , find_packages
+
+setup(
+    name='packageSensitiveInfo',
+    version="1.0.0",
+    author='Simran',
+    author_email='simran.saxena@dataverze.ai',
+    description='This package is used to get the employee details like employees whose date_of_joining is greater than input date_of_joining and average salary of empolyees of a particular designation.',
+    packages=find_packages(),
+    #namespace_packages=['packageSensitiveInfo'],
+    #package_dir={'packageSensitiveInfo': 'src'},
+    install_requires=[
+        'black==24.3.0',
+        'pytest==8.0.2',
+        'setuptools==69.1.1',
+        'wheel==0.42.0',
+        'twine==5.0.0',
+    ],
+    package_data={'employee_insights': ['logs/*', 'tests/*.py']},
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    
+)
